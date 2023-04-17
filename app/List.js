16f8eaa47ece605ebd8013a4b28b08328ecd0003
Rokas <rookas.rudzenskas@gmail.com>
2023-04-17 10:09:42 +0300

    Add volume control to MusicPlayer componentThis commit adds a volume control feature to the MusicPlayer component, allowing users to select their desired volume using a slider. The selected volume is also passed to the VolumeContext to update the application-wide volume state

---
+++ app/List.js
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View } from 'react-native';
+import MusicPlayer from '../src/components/MusicPlayer';
+import Slider from '@react-native-community/slider';
+import { useVolume } from '../src/contexts/VolumeContext';
+import { FontAwesome } from '@expo/vector-icons';
+import {useEffect, useState} from "react";
+
+export default function Template() {
+  const { volume, setVolume } = useVolume();
+  const [selectedValue, setSelectedValue] = useState(50);
+
+  useEffect(() => {
+    setVolume(selectedValue / 100);
+  }, [selectedValue]);
+
+  return (
+    <View style={styles.container}>
+      <MusicPlayer />
+      <Text style={{color: 'white', fontSize: 18, margin: 5, marginLeft: 15}}>Select the desired volume</Text>
+      <View style={{flexDirection: 'row', flexWrap: 'wrap', alignContent: 'center', justifyContent: 'center'}}>
+        {Array(100).fill(0).map((i, index) => (
+          <Text
+            onPress={() => setSelectedValue(index)}
+            style={{
+              backgroundColor: selectedValue === index ? 'magenta' : 'transparent',
+              color: 'white', borderColor: 'white', borderWidth: 1, width: 30, height: 30, margin: 5, textAlign: 'center', padding: 5
+          }} key={index}>{index}</Text>
+        ))}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+});

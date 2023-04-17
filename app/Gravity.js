c6e4e8990534797eefe4f307f14b638eb24e3227
Rokas <rookas.rudzenskas@gmail.com>
2023-04-17 11:35:55 +0300

    "Update Gravity.js to use sensor input to adjust shared volume in real-time."

--- app/Gravity.js
+++ app/Gravity.js
@@ -28,16 +28,18 @@ export default function Template() {
       return;
     }
 
-    acceleration.value += sensor.sensor.value.x * dt * 0.0001;
+    sharedVolume.value += sensor.sensor.value.x * dt * 0.0001;
 
-    sharedVolume.value += acceleration.value * dt * 0.001;
-
-    sharedVolume.value = Math.min(sharedVolume.value, 1);
-    sharedVolume.value = Math.max(sharedVolume.value, 0);
-
-    if (sharedVolume.value === 0 || sharedVolume.value === 1) {
-      acceleration.value = 0;
-    }
+    // acceleration.value += sensor.sensor.value.x * dt * 0.0001;
+    //
+    // sharedVolume.value += acceleration.value * dt * 0.001;
+    //
+    // sharedVolume.value = Math.min(sharedVolume.value, 1);
+    // sharedVolume.value = Math.max(sharedVolume.value, 0);
+    //
+    // if (sharedVolume.value === 0 || sharedVolume.value === 1) {
+    //   acceleration.value = 0;
+    // }
   });
 
   const sliderViewStyle = useAnimatedStyle(() => {
